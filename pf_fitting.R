35831403bf74ddf88da26789515dc232fbf8ba79
yannansoda <yannansusoda@gmail.com>
2020-06-19 14:25:40 +0200

    add PF fitting R scripts

---
+++ pf_fitting.R
@@ -0,0 +1,27 @@
+library(readxl)
+library(quickpsy)
+library(dplyr)
+
+# import and merge data
+setwd("/media/yannan/Lethe/PhD-projects/staircase_methods")
+all_files <- list.files(path="data/ysu_lab_quest", pattern = "*.xlsx", full.names = T)
+df_lists <- list() 
+
+
+for (i in 1:length(all_files)){
+  all_labels <- openxlsx::getSheetNames(all_files[i])
+  df_lists[[i]]<- lapply(all_labels,openxlsx::read.xlsx,xlsxFile=all_files[i])
+  names(df_lists[[i]]) <- all_labels
+}
+data = do.call(Map, c(f = rbind, df_lists))
+data = data[order(names(data))]
+
+# extract data for fitting
+for (dt in data){
+  df = select(dt, All.Intensities, All.Responses)
+  fit <- quickpsy(df, All.Intensities, All.Responses, guess = 0.5)
+  fit %>% plot()
+}
+
+# df_group = aggregate(data = df1,All.Responses ~ All.Intensities, FUN=paste)
+# TODO: structure the data to what quickpsy asks
